c591e7e37ed277d783d714597e556f229c12aec1
adamgado <65188827+adamgado@users.noreply.github.com>
2024-12-21 16:28:55 +0200

    add register route that user data is posted to

--- web_app/views.py
+++ web_app/views.py
@@ -1,10 +1,17 @@
 from web_app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    return render_template('main.html')
     
+@app.route('/register', methods=['Post'])
+def register_page():
+    username = request.form['name']
+    email_address = request.form['email-address']
+    password = request.form['password']
+    return username + " registered successfully"
+
 @app.route('/quizzes')
 def quiz_list():
     return 'list of quizzes'
