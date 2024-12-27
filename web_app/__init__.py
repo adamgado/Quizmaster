8eb3a0d9aae486aa7bf167596f547e8e2c935b22
adamgado <65188827+adamgado@users.noreply.github.com>
2024-12-27 20:27:49 +0200

    add a db object in init file

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
+app.config["SECRET_KEY"] = "w648v87qny487q"
+app.config["SQLALCHEMY_DATABASE_URL"] = 'sqlite:///users_informations.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+db = SQLAlchemy(app)
 
 from web_app import views
+from web_app import models
 
 app.config.from_object('configuration')
